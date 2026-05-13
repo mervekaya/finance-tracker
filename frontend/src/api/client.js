9f3742ca3e811ddaa012fd3611a47034662416c0
Merve KAYA ZEYBEL <kaya.merve@tubitak.gov.tr>
2026-05-13 10:08:04 +0300

    use env var for api url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8000" });
+const client = axios.create({ baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000" });
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
